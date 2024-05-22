d678f1de39dbecb6801df6f67d13350ddacef297
Mauricio Costa <169109064+MauricioCostaBarros@users.noreply.github.com>
2024-05-22 20:25:27 +0000

    Primeiro Componente

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Cabecalho from './componentes/Cabecalho';
 
 function App() {
   return (
     <div className="App">
       <h1>Mauricio Costa Barros</h1>
-    teste
+      <Cabecalho>
+        
+      </Cabecalho>
     </div>
   );
 }
